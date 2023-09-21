8b5e102f4a0ca9940230119ebe21097c4dcacbaa
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-21 16:56:28 +0800

    导航设计

--- pages/_layout.tsx
+++ pages/_layout.tsx
@@ -1,14 +1,39 @@
-import { Layout } from 'antd';
+import { Button, Layout, Menu, Space } from 'antd';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import style from '../styles/layout.module.scss';
 
 const { Header, Content } = Layout;
+
+export
+function NavMenu() {
+  return <nav className={style.navmenu}>
+    <ol>
+      <li>
+        <span>Game</span>
+      </li>
+      <li>
+        <span>Swap</span>
+      </li>
+      <li>
+        <span>Finance</span>
+      </li>
+    </ol>
+  </nav>
+}
 
 export default ((props: any) => {
   const Component = props.Component;
   const pageProps = props.pageProps;
   return <Layout className="App">
     <Header className="Header">
-      <h1>EmojiWorld</h1>
+      <Space>
+        <h1
+          className={style.logo}
+          style={{ marginRight: '1.2rem' }}>
+          Em<span className={style.haha}>😄</span>ji
+        </h1>
+        <NavMenu />
+      </Space>
       <ConnectButton />
     </Header>
     <Content className="Content">
