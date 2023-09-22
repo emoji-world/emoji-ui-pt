277f275ed0e4a73a9837f415878b929c66e1a7f0
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-22 11:58:27 +0800

    昨天的代码提交

---
+++ components/embutton/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import style from './index.module.scss';
+
+export default
+function EButton(props: any) {
+  return <span className={style.com}>😄</span>;
+}
--- components/swaper/index.tsx
+++ components/swaper/index.tsx
@@ -1,8 +1,30 @@
+import { Card, Col, Input, Row } from 'antd';
 import React from 'react';
+import style from './index.module.scss';
+import EButton from '../embutton';
 
 export
 function Swaper() {
-  return <div>
-    <span>Swaper</span>
+  return <div className={style.com}>
+    <Row>
+      0
+    </Row>
+    <Row className={style.source}>
+      <Col span={24}>
+        <Card>
+          <Input />
+        </Card>
+      </Col>
+    </Row>
+    <Row>
+      <EButton />
+    </Row>
+    <Row className={style.target}>
+      <Col span={24}>
+        <Card>
+          1
+        </Card>
+      </Col>
+    </Row>
   </div>;
 }
