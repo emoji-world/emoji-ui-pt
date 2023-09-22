2c1200c3c1e8200c3eceaac6e22aab9ad938492f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-22 13:51:21 +0800

    调整样式

--- components/swaper/index.tsx
+++ components/swaper/index.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Input, Row } from 'antd';
+import { Button, Card, Col, Input, InputNumber, Row, Slider } from 'antd';
 import React from 'react';
 import style from './index.module.scss';
 import EButton from '../embutton';
@@ -6,25 +6,36 @@ import EButton from '../embutton';
 export
 function Swaper() {
   return <div className={style.com}>
-    <Row>
-      0
-    </Row>
-    <Row className={style.source}>
-      <Col span={24}>
-        <Card>
-          <Input />
-        </Card>
-      </Col>
-    </Row>
-    <Row>
-      <EButton />
-    </Row>
-    <Row className={style.target}>
-      <Col span={24}>
-        <Card>
-          1
-        </Card>
-      </Col>
-    </Row>
+    <div></div>
+    <div className={style.card}>
+      <div className={style.input_area}>
+        <input type="number" className={style.amount_input} placeholder="0.0" />
+        <Button size="large" type="primary">ETH</Button>
+      </div>
+      <div className={style.suppl_info}>
+        <span></span>
+        <span className={style.balance}>Balance {0.03}</span>
+      </div>
+      <div className={style.percent_btns}>
+        <Button type="primary" ghost>25%</Button>
+        <Button type="primary" ghost>50%</Button>
+        <Button type="primary" ghost>75%</Button>
+        <Button type="primary" ghost>100%</Button>
+      </div>
+      <div className={style.percent_slider}>
+        <Slider defaultValue={30} tooltip={{ placement: 'bottom' }} />
+      </div>
+    </div>
+    <div className={style.card}>
+      <div className={style.input_area}>
+        <input type="number" className={style.amount_input} placeholder="0.0" />
+        <Button size="large" type="primary">USDC</Button>
+      </div>
+      <div className={style.suppl_info}>
+        <span></span>
+        <span className={style.balance}>Balance {0.03}</span>
+      </div>
+    </div>
+    <div className={style.card}>3</div>
   </div>;
 }
