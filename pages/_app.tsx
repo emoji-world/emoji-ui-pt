1811c9fd91620301d7c8e45eb95f4ef5a7a81ec0
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-25 15:10:13 +0800

    定制钱包

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.scss';
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit';
+import { RainbowKitProvider, lightTheme, connectorsForWallets } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
@@ -17,6 +17,19 @@ import { ConfigProvider } from 'antd';
 import Layout from '../layout';
 import { useEffect } from 'react';
 import { merge } from '../utils/tools';
+import {
+  metaMaskWallet,
+  coinbaseWallet,
+  okxWallet,
+  rainbowWallet,
+  walletConnectWallet,
+  injectedWallet,
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+  omniWallet,
+  imTokenWallet,
+} from '@rainbow-me/rainbowkit/wallets';
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
@@ -31,11 +44,33 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
   [publicProvider()]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains,
-});
+const appName = 'RainbowKit App';
+const projectId = 'YOUR_PROJECT_ID';
+const walletOptions = { chains, appName, projectId };
+
+const connectors = connectorsForWallets([
+  {
+    groupName: 'Popular',
+    wallets: [
+      metaMaskWallet(walletOptions),
+      coinbaseWallet(walletOptions),
+      okxWallet(walletOptions),
+      rainbowWallet(walletOptions),
+      walletConnectWallet(walletOptions),
+      injectedWallet(walletOptions),
+    ],
+  },
+  {
+    groupName: 'Suggested',
+    wallets: [
+      argentWallet(walletOptions),
+      trustWallet(walletOptions),
+      ledgerWallet(walletOptions),
+      omniWallet(walletOptions),
+      imTokenWallet(walletOptions),
+    ],
+  },
+]);
 
 const wagmiConfig = createConfig({
   autoConnect: true,
