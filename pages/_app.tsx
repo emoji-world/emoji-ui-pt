229e04db74afcdc04ef6ca41638a70191cc973e0
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-25 15:15:37 +0800

    定制链

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,7 @@ import type { AppProps } from 'next/app';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
   arbitrum,
-  goerli,
-  mainnet,
-  optimism,
-  polygon,
-  base,
-  zora,
+  arbitrumGoerli,
 } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 import { ConfigProvider } from 'antd'; 
@@ -33,13 +28,9 @@ import {
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
-    mainnet,
-    polygon,
-    optimism,
     arbitrum,
-    base,
-    zora,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+    arbitrumGoerli,
+    // ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
   ],
   [publicProvider()]
 );
