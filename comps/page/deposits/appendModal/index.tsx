a09cc5af728e0e3e303500f3b963a6c3dae030d6
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-11 22:45:09 +0800

    提交代码

--- comps/page/deposits/appendModal/index.tsx
+++ comps/page/deposits/appendModal/index.tsx
@@ -1,8 +1,8 @@
 import { Form, Modal, ModalProps, message } from 'antd';
 import TokenAmount from '../../../common/TokenAmount';
-import { useContractWrite } from 'wagmi';
+import { useAccount, useBalance, useContractWrite } from 'wagmi';
 import { abi } from '../../../../contracts/JIMAO.json';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const address = '0x527C0b26D899A3Bc7d232ADFb4B771cD3F1c4910';
 
@@ -14,9 +14,24 @@ interface IProps extends ModalProps {
 
 export default
 function AppendModal(props: IProps) {
+  const [open, setOpen] = useState<boolean>(false);
   const [form] = Form.useForm();
+  const account = useAccount();
+  const balance = useBalance({ address: account.address });
 
-  const withdrawETH = useContractWrite({
+  const openModal = async () => {
+    const latestBalance = await balance.refetch();
+    form.resetFields();
+    form.setFieldValue('amount', latestBalance.data?.value ?? 0n);
+    setOpen(true);
+  };
+
+  useEffect(() => {
+    if (props.open) openModal();
+    else setOpen(false);
+  }, [props.open]);
+
+  const appendETH = useContractWrite({
     abi,
     address,
     functionName: 'changeDepositETH',
@@ -25,44 +40,42 @@ function AppendModal(props: IProps) {
     },
   });
 
-  const handleWithdrawETH = async (event: any) => {
+  const handleAppendETH = async (event: any) => {
     try {
       const data = await form.validateFields();
-      const txn = await withdrawETH.writeAsync({ args: [props.open.index, data.amount] });
-      message.success('Withdraw has submitted');
-      props.onNewTxn?.({ hash: txn.hash, name: 'Withdraw ETH' });
+      const txn = await appendETH.writeAsync({
+        args: [props.open.index, 0],
+        value: data.amount,
+      });
+      message.success('Append has submitted');
+      props.onNewTxn?.({ hash: txn.hash, name: 'Append ETH' });
       props.onCancel?.(event);
     } catch (error) {
       console.error(error);
     }
   };
 
-  useEffect(() => {
-    if (props.open) {
-      form.resetFields();
-      form.setFieldsValue({
-        amount: props.open?.amount ?? 0n,
-      });
-    }
-  }, [props.open]);
-
   return <Modal
     { ...props }
+    open={open}
     title="AppendModal"
     maskClosable={false}
     okButtonProps={{
-      loading: withdrawETH.isLoading,
+      loading: appendETH.isLoading,
     }}
-    onOk={handleWithdrawETH}>
+    onOk={handleAppendETH}>
     <Form
       form={form}
       layout="vertical">
       <Form.Item label="Amount" name="amount">
         <TokenAmount
           symbol="ETH"
-          balance={(props.open?.amount ?? 0n)}
-          precision={18}
+          balance={(balance.data?.value ?? 0n)}
+          precision={Number(balance.data?.decimals ?? 0)}
           precisionShow={4}
+          showBalanceLoading
+          balanceLoading={balance.isLoading}
+          onUpdateBalance={() => balance.refetch()}
         />
       </Form.Item>
     </Form>
